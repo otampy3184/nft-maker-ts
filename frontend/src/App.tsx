b5f4ff9f00e0c8c223f811f9293618fd339ff2b0
django_freeman <o.tampodo.t.18@gmail.com>
2023-01-09 00:21:09 +0900

    hooksと子コンポーネント用の操作関数追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,18 @@ import { ConnectWallet } from './components/ConnectWallet';
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<string>("")
+  const [openseaLink, setOpenseaLink] = useState<string>("");
+  const [name, setName] = useState<string>("")
+  const [description, setDescription] = useState<string>("")
+
   const setterAccount = (account : string)=> setCurrentAccount(account)
+  const setterIsLoading = (isloading: boolean) => setIsLoading(isloading)
+  const setterResult = (result: string) => setResult(result)
+  const setterOpenseaLink = (opensealink: string) => setOpenseaLink(opensealink)
+  const setterName = (name: string) => setName(name)
+  const setterDescription = (description: string) => setDescription(description)
 
   return (
     <div className="App">
