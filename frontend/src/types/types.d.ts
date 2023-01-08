5bb12d217eeada0e652f0cacf96815e9db8a9081
django_freeman <o.tampodo.t.18@gmail.com>
2023-01-09 00:20:45 +0900

    型定義を整理

---
+++ frontend/src/types/types.d.ts
@@ -0,0 +1,26 @@
+interface Window {
+  ethereum: any;  // 苦肉の策
+}
+
+interface WalletProps {
+  currentAccount: string;
+  setterAccount: (account: string) => void;
+}
+
+interface IPFSProps {
+  result: string
+  setterIsLoading: (isLoading: boolean) => void
+  setterResult: (result: string) => void
+}
+
+interface MintProps {
+  name: string
+  description: string
+  openseaLink: string
+  result: string
+  setterName: (name: string) => void
+  setterDescription: (description: string) => void
+  setterOpenseaLink: (openseaLink: string) => void
+  setterResult: (result: string) => void
+  setterIsLoading: (isLoading: boolean) => void
+}
