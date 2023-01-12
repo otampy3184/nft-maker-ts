35b352eb44efdf4eb501b4d4d5cc6d0551c8437e
django_freeman <o.tampodo.t.18@gmail.com>
2023-01-12 22:39:06 +0900

    MintNFT追加

--- frontend/src/components/MintNFT.tsx
+++ frontend/src/components/MintNFT.tsx
@@ -1,13 +1,35 @@
 import { Button } from "@mui/material";
 import React, { useEffect } from "react";
 
-type NFTProps = {
-  currentAccount: string
-  setterAccount: () => (account: string) => void;
-}
+import { ethers } from 'ethers'
 
-export function MintNFF(props: NFTProps) {
-  const test: string = props.currentAccount
+import NFTMaker from "../abi/NFTMaker_mumbai.json"
+const CONTRACT_ADDRESS = "0xb57D3b3c1C8C18b47Aa8d04d795f0c2fB7622c30"
+
+export function MintNFT(props: MintProps) {
+  const test: string = props.name
+  const mintNFT =async () => {
+    try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new  ethers.Contract(
+            CONTRACT_ADDRESS,
+            NFTMaker.abi,
+            signer
+        )
+        const nftTxn = await contract.mintNFT(props.name, props.result, props.description, {
+            gasLimit: 300000,
+        })
+        props.setterIsLoading(true)
+        await nftTxn.wait()
+        console.log(
+            `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        )
+        props.setterIsLoading(false)
+    } catch (error){
+        console.log(error)
+    }
+  }
   return (
     <div>
 
